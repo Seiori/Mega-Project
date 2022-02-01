f2ce0a5a674c8b8204eb2913089c51dcd2d22b5f
daneg <daneg@DESKTOP-7J8HTN0>
2022-02-01 21:38:29 +0000

    Add project files.

---
+++ Mega Project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mega_Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to my Mega Project!");
+            Console.WriteLine();
+            Console.WriteLine("Please Select one of the Following");
+            Console.WriteLine();
+            Console.WriteLine("1.  Numbers");
+            Console.WriteLine("2.  Classic Algorithns");
+            Console.WriteLine("3.  Graph");
+            Console.WriteLine("4.  Data Structures");
+            Console.WriteLine("5.  Text");
+            Console.WriteLine("6.  Networking");
+            Console.WriteLine("7.  Classes");
+            Console.WriteLine("8.  Threading");
+            Console.WriteLine("9.  Web");
+            Console.WriteLine("10. Files");
+            Console.WriteLine("11. Databases");
+            Console.WriteLine("12. Graphics and Multimedia");
+            Console.WriteLine("13. Security");
+            Console.WriteLine();
+
+            Console.WriteLine("Please Enter the Corresponding Number to the Option of your Choice");
+            string Choice = Console.ReadLine();
+
+
+        }
+    }
+}
